e5e4d2f8694e0cd03a3ee62f7c699152ec4b5345
Enrico <enrico.melis.casa@gmail.com>
2023-03-08 13:04:48 +0100

    finished classe strumenti

--- oop/classStrumenti.cpp
+++ oop/classStrumenti.cpp
@@ -6,24 +6,27 @@ using namespace std;
 
 class Strumenti {
     protected: 
+        string nome;
         string materiale;
         float costo;
         string scala; 
     public: 
-        Strumenti(string mat, float cos, string sca);
+        Strumenti(string nom, string mat, float cos, string sca);
         void stampa();
         ~Strumenti();
 };
 
-Strumenti::Strumenti(string mat, float cos, string sca){
+Strumenti::Strumenti(string nom, string mat, float cos, string sca){
+    nome = nom;
     materiale = mat;
     costo = cos;
     scala = sca;
 }
 
 void Strumenti::stampa(){
-    cout << "Materiale: " << materiale << 
-    endl << "Costo: " << costo << 
+    cout << "Nome: " << nome <<
+    endl << "Materiale: " << materiale << 
+    endl << "Costo: " << costo << "€" <<
     endl << "Scala: " << scala;
 }
 
@@ -35,17 +38,44 @@ class Corda : public Strumenti{
     protected: 
         int n_corda;
     public: 
-        Corda(string mat, float cos, string sca, int num);
+        Corda(string nom, string mat, float cos, string sca, int num);
         void stampa();
 };
 
-Corda::Corda(string mat, float cos, string sca, int num): Strumenti(mat, cos, sca){
+Corda::Corda(string nom, string mat, float cos, string sca, int num): Strumenti(nom, mat, cos, sca){
     n_corda = num;
 }
 
 void Corda::stampa(){
-    cout << "Materiale: " << materiale << 
-    endl << "Costo: " << costo << 
+    cout << "Strumento a corda: " << endl << "---------------" << endl;
+    cout << "Nome: " << nome <<
+    endl << "Materiale: " << materiale << 
+    endl << "Costo: " << costo << "€" << 
     endl << "Scala: " << scala <<
     endl << "Numero corde: " << n_corda;
+}
+
+int main(){
+    string nome_strumento, materiale_strumento, scala_strumento;
+    int numero_corde;
+    float prezzo_strumento;
+
+    cout << "Strumento a corda: " << endl << "----------------------" << endl;
+    cout << "Inserire il nome dello strumento: ";
+    cin >> nome_strumento;
+    cout << "Inserire il materiale dello strumento: ";
+    cin >> materiale_strumento;
+    cout << "Inserire il costo dello strumento: ";
+    cin >> prezzo_strumento;
+    cout << "Inserire la scala suonata: ";
+    cin >> scala_strumento;
+    cout << "Inserire il numero delle corde: ";
+    cin >> numero_corde;
+
+    class Corda strumento(nome_strumento, materiale_strumento, prezzo_strumento, scala_strumento, numero_corde);
+    cout << endl;
+    strumento.stampa();
+
+    cout << endl;
+    return 0;
 }
