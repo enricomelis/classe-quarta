fc24ef00b09aea620e244110f5088b991b54834b
Enrico <enrico.melis.casa@gmail.com>
2023-04-07 20:09:51 +0200

    started wordcount.js

---
+++ oop/js-playground/wordcount.js
@@ -0,0 +1,23 @@
+// let input = prompt("Enter a phrase: ");
+
+let phrase = "Hi, my name is Enrico. My height is 180";
+let words = phrase.split(/\W+/);
+let lowerWords = words.map(word => word.toLowerCase());
+console.log(lowerWords);
+
+let counter = 0;
+let wordCounter = [];
+
+for (let i = 0; i < lowerWords.length; i++) {
+    for (let j = 0; j<lowerWords.length; j++){
+        if (lowerWords[i] == lowerWords[j]) {
+            counter++;
+        }
+    }
+    counter = 0;
+}
+
+console.log(wordCounter);
+
+
+
