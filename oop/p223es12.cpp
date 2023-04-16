27fa4894360ff7c5a84d93cb8616d7b5a3eeaa60
Enrico <enrico.melis.casa@gmail.com>
2023-04-16 17:16:39 +0200

    Update p223es12.cpp

--- oop/p223es12.cpp
+++ oop/p223es12.cpp
@@ -7,34 +7,57 @@ using namespace std;
 
 class Pizza {
     protected: 
+        string nome;
         map <string, string> ingredienti;
         int calorieTotali;
         float prezzo;
     public:
-        Pizza(int calTot, float price); 
-
-        void getIngredienti();
-        string getTipoIngrediente(string ingrediente) { return ingredienti[ingrediente]; }
+        Pizza(string name, int calTot, float price); 
+        // tutte le get();
+        void stampa(); // get globale
+        void stampaIngredienti(); 
         int getCalorieTotali() { return calorieTotali; }
-
+        float getPrezzo() { return prezzo; }
+        string getNome() { return nome; }
+        string getOrigineIngrediente(string ingrediente) { return ingredienti[ingrediente]; }
+        // tutte le set();
         void setIngredienti();
+        void removeIngrediente(string remove);
         void setCalorieTotali(int nuovo) { calorieTotali = nuovo; }
         void setPrezzo(int nuovo) { prezzo = nuovo; }
-
-
-        float getPrezzo() { return prezzo; }
-
         ~Pizza() { cout << "distruttore."; }
 };
 
 int main(){
+    int kcal_pizza, prezzo_pizza;
+    string nome_pizza;
 
-    Pizza prima_pizza(800, 8);
-    cout << "La prima pizza ha " << prima_pizza.getCalorieTotali() << " kcal." << endl << endl;
+    cout << "Inserire nome della pizza: ";
+    cin >> nome_pizza;
+
+    do {
+        cout << "Inserire calorie della pizza: ";
+        cin >> kcal_pizza;
+    } while(kcal_pizza < 0);
+
+    do {
+        cout << "Inserire prezzo della pizza: ";
+        cin >> prezzo_pizza;
+    } while(prezzo_pizza < 0);
+    
+
+    Pizza prima_pizza(nome_pizza, kcal_pizza, prezzo_pizza);
     prima_pizza.setIngredienti();
 
-    cout << endl << "----------- Gli ingredienti e la loro origine -----------" << endl;
-    prima_pizza.getIngredienti();
+    string da_rimuovere;
+    cout << "Inserire ingrediente da rimuovere: ";
+    cin >> da_rimuovere;
+    prima_pizza.removeIngrediente(da_rimuovere);
+
+    prima_pizza.stampa();
+
+    cout << endl << endl;
+    prima_pizza.stampa();
 
 
     cout << endl;
@@ -43,34 +66,48 @@ int main(){
 
 // definizione metodi classe Pizza
 
-Pizza::Pizza(int calTot, float price){
+Pizza::Pizza(string name, int calTot, float price){
+    nome = name;
     calorieTotali = calTot;
     prezzo = price;
 }
 
-void Pizza::getIngredienti(){
+void Pizza::stampaIngredienti(){
     for (auto element: ingredienti){
-        cout << element.first << ": " << element.second << endl;
+        cout << element.first << " (" << element.second << ")" << endl;
     }
+    // element.first = chiave, element.second = valore
 }
 
 void Pizza::setIngredienti(){
-    bool stop = true;
-    string ingrediente, tipo, risposta;
+    bool stop = false;
+    string ingrediente, origine;
 
-    while(stop){
-        //ipotizzo inizialmente che gli input siano sempre coerenti, 
-        // il controllo lo farò poi
+    cout << "Inserimento ingredienti (ingrediente = 0 per terminare):  " << endl;
+    while(!stop){
         cout << "Inserire ingrediente: ";
         cin >> ingrediente;
-        cout << "Inserire tipo ingrediente: ";
-        cin >> tipo;
 
-        ingredienti[ingrediente] = tipo;
-        cout << "Continuare l\'inserimento? (s/n)";
-        cin >> risposta;
-        if(risposta == "n"){
-            stop = false;
+        if(ingrediente == "0"){
+            stop = true;
+        }
+        else {
+            cout << "Inserire origine ingrediente: ";
+            cin >> origine;
+            ingredienti[ingrediente] = origine;
         }
     }
+}
+
+void Pizza::removeIngrediente(string remove){
+    ingredienti.erase(remove);
+}
+
+void Pizza::stampa(){
+    cout << "-------- ";
+    cout << "Pizza: " << nome << 
+    endl << "Calorie: " << calorieTotali <<
+    endl << "Prezzo: " << prezzo << " euro" <<
+    endl << "Ingredienti: " << endl;
+    stampaIngredienti();
 }
